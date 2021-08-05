bb07dfcb29068c8162f5357531dc0c8b288bed15
Farhan M R A <48043106+farhanmra@users.noreply.github.com>
2021-08-05 18:56:51 +0700

    commit
    commit

--- Day 2/day2.js
+++ Day 2/day2.js
@@ -42,3 +42,102 @@ switch(true){
     default:
         document.write('Grade D')
 }
+
+// var angka=0;
+
+// while(angka<=5){
+//     document.write('angka '+angka +'<br>')
+//     angka+=1;
+// }
+
+// var angka=1
+// for(angka;angka<=10;angka++){
+//     for (angka;angka<=5;angka++) {
+//     document.write('siswa dengan no induk '+angka+' TA Vista'+'<br>')}
+//     for (angka;angka<=10;angka++){
+//         document.write('siswa dengan no induk '+angka+' TA Intan'+'<br>')
+//     }
+// }
+
+// var angka=1
+// while(angka<=5){
+//     document.write('siswa dengan no induk '+angka+' TA Vista'+'<br>')
+//     angka+=1;
+// }
+
+
+// for (angka=6;angka<=10;angka++){
+//     document.write('siswa dengan no induk '+angka+' TA Intan'+'<br>')
+// }
+
+// buah=['apel','pisang','belimbing']
+// document.writeln(typeof(buah))
+// document.write('<br>')
+// document.writeln(buah.length)
+// document.write('<br>')
+// document.writeln(buah[1])
+// document.write('<br>')
+
+// inifungsi=function(){}
+// array=['teks',1234,true,inifungsi,[2,3,5]]
+// document.write('<br>')
+// document.write(array[4][2]) 
+
+// array2=[]
+// array2[0]='senin';
+// array2[1]='selasa';
+// array2[3]='kamis';
+
+// buah=['apel','pisang','belimbing','salak','anggur']
+//1.join
+// document.write(buah.join('<br>'))
+// 2.push
+// push menambahkan element di akhir
+// buah.push('duren')
+// document.write(buah.join('<br>'))
+// 3.pop 
+// menghapus elment
+// buah.pop()
+// document.write(buah.join('<br>'))
+//4.unshift
+//digunakan untuk menambahkan array di awal element
+// buah.unshift('jambu')
+// document.write(buah.join('<br>'))
+//5.shift
+//menghilangkan element diawal array
+// buah.shift()
+// document.write(buah.join('<br>'))
+//6.splice
+//splice menambahkan dan menghapus di tengah array
+//splice (index awal,jmlHapus,elementBaru1,...,elementBaruN)
+// buah.splice(2,2,'kelapa','melon')
+// document.write(buah.join('<br>'))
+
+//7. Slice
+// newBuah=buah.slice(2,4)
+// document.write('<br>')
+// document.write('<br>')
+// document.write(newBuah.join('<br>')) 
+
+//8. for each
+// buah.foreach(function(e,i)){
+//     document.write(i+' = ' +e+'<br>')
+// }
+
+// buah.foreach(function(e,i)){
+//     document.write((i+1) +' = ' +e+'<br>')
+// }
+
+// //9. map
+// // mengembalikan array
+// var buahBaru=buah.map(function(e,i){
+//     return  e + ' enak dimakan'
+// })
+// document.write('<br>')
+// document.write('<br>')
+
+
+
+
+
+
---
+++ Day 3/day3.js
@@ -0,0 +1,11 @@
+var angka1=parseInt(prompt("masukan sisi 1 :"));
+var angka2=parseInt(prompt("masukan sisi 2 :"));
+
+function kubus1(angka1){
+    jumlah=angka1 *angka1 *angka1
+    return jumlah
+}
+function kubus2(angka2){
+    jumlah=angka1 *angka1 *angka1
+    return jumlah
+}
